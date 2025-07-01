2069f5b3c2ac3eb5125a96e64245c821710dc70e
kangtoe <kangtoe99@gmail.com>
2025-07-01 09:33:19 +0900

    3D 카메라 제어 기능 추가

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,24 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    [SerializeField] Camera _camera;
+    [SerializeField] Camera _camera => Camera.main;
+    
+    [Header("카메라 설정")]    
+    [SerializeField] Vector3 lookAtOffset = new Vector3(0, 0, 0);
+    [SerializeField] Vector3 cameraOffset = new Vector3(0, -6, -10);
+    [SerializeField] Vector3 allOffset = new Vector3(0, 0, 0);
+    [SerializeField] float smoothSpeed = 5f;
+    [SerializeField] float fov = 60f;
+    
+    Vector3 currentVelocity = Vector3.zero;
 
-    [SerializeField] float _angle = 0f;
-    [SerializeField] Vector3 _offset = new Vector3(0, 0, -10);
+    Vector3 lookAtPosition => BoardManager.Instance.BoardCenter + lookAtOffset + allOffset;
+    Vector3 cameraPosition => BoardManager.Instance.BoardCenter + cameraOffset + allOffset;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        _camera.transform.position = BoardManager.Instance.BoardCenter + _offset;
-        _camera.transform.rotation = Quaternion.Euler(_angle, 0, 0);
+        if (BoardManager.Instance == null) return;
+        
+        // 목표 위치 계산
+        Vector3 targetPosition = cameraPosition;        
+        
+        // 부드러운 이동
+        Vector3 smoothedPosition = Vector3.SmoothDamp(_camera.transform.position, targetPosition, ref currentVelocity, 1f / smoothSpeed);
+        _camera.transform.position = smoothedPosition;
+        
+        // 보드 중앙을 바라보기
+        _camera.transform.LookAt(lookAtPosition);
+        
+        // FOV 설정
+        _camera.fieldOfView = fov;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(lookAtPosition, 0.2f);
+
+        GUIStyle style = new GUIStyle();
+        style.alignment = TextAnchor.MiddleCenter;
+        style.fontSize = 18;
+        style.normal.textColor = Color.magenta;
+        
+        #if UNITY_EDITOR
+        UnityEditor.Handles.Label(lookAtPosition + Vector3.up * 0.3f, "lookAtPosition", style);
+        #endif
     }
 }
