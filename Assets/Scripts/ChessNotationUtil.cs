3cbb0849afd007ae222c92053dd8226185730348
kangtoe <kangtoe99@gmail.com>
2025-07-26 00:12:04 +0900

    기물 움직임 이후 FEN을 구하는 함수 ChessNotationUtil에 추가

--- Assets/Scripts/ChessNotationUtil.cs
+++ Assets/Scripts/ChessNotationUtil.cs
@@ -142,4 +142,144 @@ public static class ChessNotationUtil
         }
         return fen.ToString();
     }
+
+    /// <summary>
+    /// 특정 움직임 적용 후 FEN을 생성합니다 (간단한 기물 이동만 처리)
+    /// </summary>
+    /// <param name="originalFen">원본 FEN</param>
+    /// <param name="moveNotation">적용할 움직임 (UCI 표기법, 예: "e2e4")</param>
+    /// <param name="currentPlayer">현재 플레이어</param>
+    /// <returns>움직임 적용 후 FEN</returns>
+    public static string GenerateFENAfterMove(string originalFen, string moveNotation, PieceColor currentPlayer)
+    {
+        if (string.IsNullOrEmpty(originalFen) || string.IsNullOrEmpty(moveNotation))
+            return originalFen;
+
+        if (moveNotation.Length < 4)
+            return originalFen;
+
+        try
+        {
+            // UCI 표기법 파싱 (예: "e2e4")
+            Vector2Int fromPos, toPos;
+            if (!TryParseChessNotation(moveNotation, out fromPos, out toPos))
+                return originalFen;
+
+            // FEN 분해 (보드 상태만 사용)
+            string[] fenParts = originalFen.Split(' ');
+            if (fenParts.Length == 0) return originalFen;
+
+            string boardState = fenParts[0];
+
+            // 보드 상태를 2차원 배열로 변환
+            char[,] board = FENToBoard(boardState);
+
+            // 기물 이동 (단순히 from에서 to로 이동)
+            char movingPiece = board[fromPos.y, fromPos.x];
+            board[fromPos.y, fromPos.x] = ' ';
+            board[toPos.y, toPos.x] = movingPiece;
+
+            // 새로운 보드 상태를 FEN으로 변환
+            string newBoardState = BoardToFEN(board);
+
+            // 턴 변경
+            string newActiveColor = currentPlayer == PieceColor.White ? "b" : "w";
+
+            // 나머지 FEN 요소들은 간단하게 처리
+            string castling = fenParts.Length > 2 ? fenParts[2] : "KQkq";
+            string enPassant = "-";
+            string halfmove = fenParts.Length > 4 ? fenParts[4] : "0";
+            string fullmove = fenParts.Length > 5 ? fenParts[5] : "1";
+
+            // 새 FEN 생성
+            return $"{newBoardState} {newActiveColor} {castling} {enPassant} {halfmove} {fullmove}";
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"FEN 생성 중 오류 발생: {e.Message}");
+            return originalFen;
+        }
+    }
+
+    /// <summary>
+    /// FEN 보드 문자열을 2차원 배열로 변환
+    /// </summary>
+    /// <param name="fenBoard">FEN 보드 부분</param>
+    /// <returns>8x8 보드 배열</returns>
+    public static char[,] FENToBoard(string fenBoard)
+    {
+        char[,] board = new char[8, 8];
+        
+        // 배열을 공백으로 초기화
+        for (int i = 0; i < 8; i++)
+            for (int j = 0; j < 8; j++)
+                board[i, j] = ' ';
+
+        string[] ranks = fenBoard.Split('/');
+        
+        for (int rank = 0; rank < 8; rank++)
+        {
+            string rankString = ranks[rank];
+            int file = 0;
+            
+            foreach (char c in rankString)
+            {
+                if (char.IsDigit(c))
+                {
+                    // 숫자는 빈 칸의 수
+                    file += c - '0';
+                }
+                else
+                {
+                    // 문자는 기물
+                    board[rank, file] = c;
+                    file++;
+                }
+            }
+        }
+        
+        return board;
+    }
+
+    /// <summary>
+    /// 2차원 배열을 FEN 보드 문자열로 변환
+    /// </summary>
+    /// <param name="board">8x8 보드 배열</param>
+    /// <returns>FEN 보드 문자열</returns>
+    public static string BoardToFEN(char[,] board)
+    {
+        string[] ranks = new string[8];
+        
+        for (int rank = 0; rank < 8; rank++)
+        {
+            string rankString = "";
+            int emptyCount = 0;
+            
+            for (int file = 0; file < 8; file++)
+            {
+                char piece = board[rank, file];
+                
+                if (piece == ' ')
+                {
+                    emptyCount++;
+                }
+                else
+                {
+                    if (emptyCount > 0)
+                    {
+                        rankString += emptyCount.ToString();
+                        emptyCount = 0;
+                    }
+                    rankString += piece;
+                }
+            }
+            
+            if (emptyCount > 0)
+                rankString += emptyCount.ToString();
+                
+            ranks[rank] = rankString;
+        }
+        
+        return string.Join("/", ranks);
+    }
 } 
